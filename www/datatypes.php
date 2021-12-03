e014f16cce4cbe92a0455493e31a62f332393a8e
Gee Hunter <gh@localhost.localdomain>
2021-12-02 19:59:41 -0500

    added datatypes

---
+++ www/datatypes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset= 'utf-8'>
+        <title></title>
+</head>
+<body>
+    <?php
+    $phrase = "To be or not to be";
+    $age = 12;
+    $gpa = 32.45;
+    $isMale = false;
+    echo $phrase;
+
+
+
+
+    ?>
+
+</body>
+
+</html>
