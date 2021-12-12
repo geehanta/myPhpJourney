da945a986aa2f89bace839c610b26fc7e1e609ab
Gee Hunter <gh@localhost.localdomain>
2021-12-12 18:44:44 -0500

    edited madlibs

--- www/gettingUserInput.php
+++ www/gettingUserInput.php
@@ -10,15 +10,17 @@
        Name: <input type="text" name="name">
        <br>
        Age: <input type="number" name="age">
+       <br>
+       School Code : <input type="text" name="code">
+       <br>
        <input type="submit">
     </form>
     <br>
-    Your name is  
-    <?php
-     echo $_GET['name']
-    ?>
+    Your name is  <?php echo $_GET['name'] ?>
     <br>
     Your age is <?php echo $_GET["age"] ?>
+    <br>
+    Your student code is : <?php echo $_GET["code"]?>
     
 
 
---
+++ www/madlibs.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset= 'utf-8'>
+        <title></title>
+</head>
+<body>
+   <form action="madlibs.php" method="get">
+    <input type="submit">
+   </form>
+    <br>
+    <?php
+     echo "Roses are {color} <br>";
+     echo "{plural noun} are blue <br>";
+     echo "I love {celebrity} <br>";
+     ?>
+
+
+</body>
+
+</html>
