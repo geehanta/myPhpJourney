83f3bb28a4af7e3c675e27e01047b9701b301558
Gee Hunter <gh@localhost.localdomain>
2021-12-12 18:57:14 -0500

    completed simple madlibs

--- www/madlibs.php
+++ www/madlibs.php
@@ -6,13 +6,20 @@
 </head>
 <body>
    <form action="madlibs.php" method="get">
+       Color: <input type="text" name="color"> <br>
+       Plural noun: <input type="text" name="pluralNoun"> <br>
+       Celebrity: <input type="text" name="celebrity"> <br>
     <input type="submit">
    </form>
     <br>
     <?php
-     echo "Roses are {color} <br>";
-     echo "{plural noun} are blue <br>";
-     echo "I love {celebrity} <br>";
+    $color = $_GET["color"];
+    $pluralNoun = $_GET["pluralNoun"];
+    $celebrity= $_GET["celebrity"];
+
+     echo "Roses are $color <br>";
+     echo "$pluralNoun are blue <br>";
+     echo "I love $celebrity <br>";
      ?>
 
 
