7a5d1160052808d41b6f53b86ce013fa3b86c8b7
Gee Hunter <gh@localhost.localdomain>
2021-12-14 15:23:23 -0500

    added a function

---
+++ www/functions.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+
+<?php
+function sayHi($name, $age){
+echo "HELLO $name AND WELCOME!!,You are $age years old!";
+}
+ 
+sayHi("HANTA",23);
+sayHi("MARK",32);
+sayHi("DAVE",22);
+?>
+</body>
+</html>
