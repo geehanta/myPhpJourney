11f8fdf3e9dfd4c0461513873fdf32d83fb7f1e0
Gee Hunter <gh@localhost.localdomain>
2021-12-14 12:41:12 -0500

    postVSget

---
+++ www/postVSget.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>postvsget</title>
+<body>
+<form action="postVSget.php" method="get">
+<input type="password" name="password"><br>
+<br>
+<input type="submit">
+<?php  echo $_GET["password"] ?>
+
+
+
+
+
+</form>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</head>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
