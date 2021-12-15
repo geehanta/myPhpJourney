d5ca96070df89c43b24e60660aa228198ef7cd27
Gee Hunter <gh@localhost.localdomain>
2021-12-15 13:38:16 -0500

    added return statement

---
+++ www/returnStatement.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+
+<?php
+function cube($num){
+return $num * $num * $num;
+ }
+$cubeResult = cube(4);
+echo $cubeResult;
+?>
+</body>
+</html>
