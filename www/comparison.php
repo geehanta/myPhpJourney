64b050c2276b058521e6f5c8246348da80901c27
Gee Hunter <gh@localhost.localdomain>
2021-12-17 14:18:12 -0500

    Added comparators

---
+++ www/comparison.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+
+<?php
+function getMax($num1, $num2){
+if($num1 > $num2){
+    return $num1;
+}else{return $num2;}
+}
+echo getMax(34,23);
+?>
+</body>
+</html>
