9b1ddcca6639b76c149b04ef583e31e1a12ec08b
Gee Hunter <gh@localhost.localdomain>
2021-12-25 02:06:40 -0500

    adding comments

---
+++ www/comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+
+<?php 
+    // this is a  line comment
+    echo "Comments are awesome!!"
+    /* 
+    this
+    is 
+    a 
+    block
+    comment
+    */
+?>
+</body>
+</html>
