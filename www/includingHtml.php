9f32bc0aa997a78f0555eebc4c3c24039af28c19
Gee Hunter <gh@localhost.localdomain>
2021-12-25 02:52:52 -0500

    included html files

---
+++ www/includingHtml.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Including Html</title>
+</head>
+<body>
+    <?php 
+    include "header.html"
+?>
+<br>
+<br>
+<br>
+<p>Hello and Welcome! Please make an order</p>
+<br>
+<br>
+<?php 
+    include "footer.html"
+?>
+</body>
+
+
+
+
+</html>
